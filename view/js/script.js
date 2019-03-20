0ee49fadb2aea458ce2aab0829afed07aed32faa
sohan1 <shn619@gmail.com>
2019-03-20 14:22:20 +0530

    done framing

--- view/js/script.js
+++ view/js/script.js
@@ -186,3 +186,17 @@ function searchVersion(pstrDataString) {
     pstrDataString.substring(lnIndex + this.versionSearchString.length + 1)
   );
 }
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  $("#" + cityName).css("display", "block");
+  evt.currentTarget.className += " active";
+}
